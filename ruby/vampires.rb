8dfbbaa78362a00bfb8ba7cef00c8d1666c92802
Matt McGee <matthew.m.mcgee@gmail.com>
2016-05-28 13:01:47 -0500

    initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,6 @@
+puts "What is your name?"
+puts "How old are you?"
+puts "What year were you born?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Would you like to enroll in the company's health insurance?"
+
