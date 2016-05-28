b668e63e4bb3a7f7e4113e8b0e37c765132ece04
Matt McGee <matthew.m.mcgee@gmail.com>
2016-05-28 15:56:16 -0500

    add allergy loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,7 +4,7 @@ i = 0
 
 def vampire_check
 	puts "What is your name?"
-	name = gets.chomp
+	name = gets.chomp.downcase
 	puts "How old are you?"
 	age = gets.chomp.to_i
 	puts "What year were you born?"
@@ -15,6 +15,7 @@ def vampire_check
 	insurance = gets.chomp
 
 	vampire = ""
+	allergies = []
 
 	# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 	if age == 2016 - year_born && (garlic == "yes" || insurance == "yes")
@@ -29,10 +30,21 @@ def vampire_check
 		vampire = "Almost certainly a vampire."
 
 	# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-	elsif name == "Drake Cula" || name == "Tu Fang"
+	elsif name == "drake cula" || name == "tu fang"
 		vampire = "Definitely a vampire"
 	else
 		puts "Results inconclusive"
+	end
+	puts "Please enter any allergies you have one at a time. Enter 'done' when finished"
+	#use a loop to ask the employee to name any allergies, one at a time. The employee can type “done” when finished.
+	#As long as the allergy is not “sunshine,” continue the loop for as long as is needed. If at any point the employee lists “sunshine” as an allergy, skip directly to the result of “Probably a vampire.”	
+	until allergies.include?("done") 
+			puts "What are you allergic to?"
+			allergies = allergies.push(gets.chomp)
+		if allergies.include?("sunshine")
+			vampire = "Probably a vampire"
+			break
+		end
 	end
 	puts vampire
 end
