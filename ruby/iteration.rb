99a8184f920cd6011df11e9bf0658764b1347fa4
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-04 15:03:12 -0700

    Create city array and guitar hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,7 @@ def say_hello
 end
 
 say_hello{ |name| puts "Hello #{name}"}
+
+
+cities = [Austin, Atlanta, San Francisco, New York City]
+famous_guitar_brands_and_type = {"Fender" => "The Strat", "Gibson" => "Les Paul", "Taylor" => "CE 10", "Martin" => "D28" }
