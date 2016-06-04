0d5bc80c1cde6f3d848b68f5527a3e0bdd89d90f
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-04 15:31:36 -0700

    Add each and map to array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -11,12 +11,20 @@ cities = ["Austin", "Atlanta", "San Francisco", "New York City"]
 famous_guitar_brands_and_type = {"Fender" => "The Strat", "Gibson" => "Les Paul", "Taylor" => "CE 10", "Martin" => "D28" }
 
 puts "Original Data" 
-puts cities
-puts famous_guitar_brands_and_type
+p cities
+p famous_guitar_brands_and_type
 
 
 cities.each do |cities| 
 	puts cities
 	
+end
+
+famous_guitar_brands_and_type.each do |"brand", "type"|
+	puts "#{brand} makes #{type} guitar!"
+end
+
+cities.map! do |cities|
+	cities << "!"
 end
 
