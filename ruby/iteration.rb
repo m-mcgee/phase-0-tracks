a1975daba0b5c4aef37afaa8170f3060d988a04d
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-04 15:08:55 -0700

    add quotation marks around cities

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,5 +1,5 @@
 def say_hello
-	name = "steve"
+	name = "Steve"
 	yield(name)
 
 end
@@ -7,5 +7,16 @@ end
 say_hello{ |name| puts "Hello #{name}"}
 
 
-cities = [Austin, Atlanta, San Francisco, New York City]
+cities = ["Austin", "Atlanta", "San Francisco", "New York City"]
 famous_guitar_brands_and_type = {"Fender" => "The Strat", "Gibson" => "Les Paul", "Taylor" => "CE 10", "Martin" => "D28" }
+
+puts "Original Data" 
+puts cities
+puts famous_guitar_brands_and_type
+
+
+cities.each do |cities| 
+	puts cities
+	
+end
+
