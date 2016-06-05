6c88f0f7ca2d31ae913eac4b56b117f0e401c47a
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-04 20:10:08 -0500

    add case statement to answer for conditional data type conversion

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -37,20 +37,52 @@ end
 # Print the hash back out to the screen 
 puts
 p client
+
 # Give the user the opportunity to update a key, If the designer says "none", skip it.
 # else take string answer and convert to a symbol 
 # use new symbol as the key to update
+# use conditional to convert to correct data type
+# if changing boolean value of "children" or "dogs" 
+# to true: get num_child or num_dogs and add to hash
+# to false: remove num_child or num_dogs from hash (as false should have no number)
+
 puts
 puts "Which key would you like to update? (Enter 'none' to exit)"
 answer = gets.chomp
 if answer != "none"
 	puts "Please update #{answer}: "
-	client[answer.to_sym] = gets.chomp
+	new_value = gets.chomp
+	case answer
+		when "name", "theme", "fav_color", "fav_shade"	
+			client[answer.to_sym] = new_value 
+		when "age", "num_child", "num_dogs"
+			client[answer.to_sym] = new_value.to_i
+		when "children"
+			if new_value == "true"
+				new_value = true
+				puts "Number of children: "
+				client[:num_child] = gets.chomp.to_i
+			else
+				new_value = false
+				client.delete(:num_child)
+			end
+			client[answer.to_sym] = new_value
+		when "dog"
+			if new_value == "true"
+				new_value = true
+				puts "Number of dogs: "
+				client[:num_dogs] = gets.chomp.to_i
+			else
+				new_value = false
+				client.delete(:num_dogs)
+			end
+			client[answer.to_sym] = new_value	
+	end	
+	p client[answer.to_sym]
 end
-
-puts
-p client
-
 
 
 # Print the latest version of the hash, and exit the program
+puts
+p client
+
