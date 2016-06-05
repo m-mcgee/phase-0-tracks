102157f7292277642b6ac4beb4ab9c529df06394
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-05 17:36:13 -0500

    create nested guitars hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,3 +4,45 @@
 #--colors array
 #--famous players/band hash
 #--coolness rating
+
+
+guitars = {
+	gibson: {
+		model_price: {
+			les_paul: 1500,
+			sg: 800,
+			es_335: 3500, 
+		},
+		colors: [
+			"black",
+			"yellow",
+			"blue"
+		],
+		coolness_rating: 10,
+	},
+	fender: {
+		model_price: {
+			stratocaster: 700,
+			telecaster: 500,
+			jaguar: 1200,
+		},
+		colors: [
+			"sunburst",
+			"red",
+			"black"
+		],
+		coolness_rating: 9,
+	},
+	jackson: {
+		model_price: {
+			rhoads: 700,
+			kelly: 800,
+			warrior: 1000,
+		},
+		colors: [
+			"black",
+			"silver"
+		],
+		coolness_rating: 4,
+	}
+} 
