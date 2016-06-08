e9c34d46208b5bfd701c8242a62ad1d34bc38e56
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-07 19:18:24 -0500

    add puppy class methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,24 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(integer)
+  	integer.times{puts "woof"}
+  end
+  def roll_over
+  	puts "*rolls over*"
+  end
+  def dog_years(years)
+  	dog_years = years * 7
+  	p dog_years
+  end
+  def play_dead
+  	puts "The dog is not moving."
+  end
 end
 
+dog = Puppy.new 
+dog.fetch("ball")
+dog.speak(3)
+dog.roll_over
+dog.dog_years(3)
+dog.play_dead
