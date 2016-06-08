b801cc590718432d4f6aace47c704dcf561dbf17
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-08 18:59:12 -0500

    initial commit

---
+++ ruby/santa.rb
@@ -0,0 +1,4 @@
+# speak method to print "Ho, ho, ho! Haaaappy holidays!" 
+# eat_milk_and_cookies method to print "That was a good <type of cookie here>!" 
+# intialize method to print "Initializing Santa instance ..."
+
