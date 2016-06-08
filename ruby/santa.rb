00e643c036cc66bccbbb07a7d5629f9ae7737a39
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-08 18:59:54 -0500

    define Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,3 +2,18 @@
 # eat_milk_and_cookies method to print "That was a good <type of cookie here>!" 
 # intialize method to print "Initializing Santa instance ..."
 
+class Santa 
+	def initialize 
+		p "Initializing Santa instance ..."
+	end
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie_type)
+		p "That was a good #{cookie_type} cookie!"
+	end
+end
+
+santa_1 = Santa.new
+santa_1.speak
+santa_1.eat_milk_and_cookies("chocolate chip")
