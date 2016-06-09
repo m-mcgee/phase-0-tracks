6a69def081339c6d8a079ada4a7dbe597f50bd3b
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-09 18:11:37 -0500

    initial commit

---
+++ ruby/solo.rb
@@ -0,0 +1,8 @@
+# Basketball player 
+#   Store player's unique name
+#   Store player's jersey number
+#   Store player's current points
+#   Give player the ability to shoot the ball
+#   Give player the ability to pass the ball
+#   	Pass ball to the correct team mate
+#   Give player the ability to slam dunk 
