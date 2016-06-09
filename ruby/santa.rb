a36f3abca0f1c76882606add8627b432933e17bb
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-09 00:31:01 -0500

    add attribute-changing methods to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,14 +14,49 @@ class Santa
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
+
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 	end
+
+	def celebrate_birthday 
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete_if {|name| name == reindeer_name}
+		@reindeer_ranking << reindeer_name
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
 end
 
-santa_1 = Santa.new("male")
-santa_1.speak
-santa_1.eat_milk_and_cookies("chocolate chip")
+st_nick = Santa.new("male", "imaginary")
+p st_nick
+st_nick.speak
+st_nick.eat_milk_and_cookies("chocolate chip")
+st_nick.gender = "agender"
+st_nick.celebrate_birthday
+p st_nick
+
+sandy_claws = Santa.new("bigender", "white")
+p sandy_claws
+sandy_claws.get_mad_at("Dasher")
+sandy_claws.ethnicity
+sandy_claws.age
+p sandy_claws
