f2e793a6f3933b40adc4879b6b75c24857489607
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-09 19:02:48 -0500

    add three point shot to shoot method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -26,8 +26,14 @@ class BasketballPlayer
 		if miss_make == 0
 			puts "#{@name} missed the shot."
 		else
-			puts "#{@name} made the shot!"
-			@points_scored += 2
+			two_three = rand(3)
+			if two_three < 2
+				puts "#{@name} made the shot!"
+				@points_scored += 2
+			else
+				puts "#{@name} made a 3!"
+				@points_scored += 3
+			end
 			puts "Total points: #{@points_scored}"
 		end
 	end
@@ -45,9 +51,10 @@ class BasketballPlayer
 end
 
 player = BasketballPlayer.new("KD", "OKC Thunder", 35, 20)
-player.slam_dunk
-player.shoot
-player.pass("Westbrook")
-p player.name
-player.jersey_number = 9
-p player.jersey_number
+# player.slam_dunk
+# player.shoot
+# player.pass("Westbrook")
+# p player.name
+# player.jersey_number = 9
+# p player.jersey_number
+
