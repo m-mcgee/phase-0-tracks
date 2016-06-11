48af9c66a06ea10974b5bda25aade82572604352
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-11 16:40:21 -0500

    add commented reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -79,4 +79,24 @@ delete_item(list, "potatoes")
 update_quant(list, "apples", 5)
 print_nice(list)
 
+# Release 5 --------Commented Reflection---------
+# What did you learn about pseudocode from working on this challenge?
+# - I need to start thinking more about what's going in and what's coming out of methods
 
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# -The array didn't seem like an appropriate choice for this challenge because each item needed to be assigned a corresponding value
+
+# What does a method return?
+# - will return a value, whether it's a string, integer, array, hash, or a result of being called on another method
+
+# What kind of things can you pass into methods as arguments?
+# - You can pass in strings, integers, arrays, hashes and other methods as arguments
+
+# How can you pass information between methods?
+# -If the second method is defined globally, you can simply call the second method within your method
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# - solidified: accessing/editing data within arrays/hashes 
+# - solidified: passing in arrays/hashes as arguments
+# - solidified: knowing how many arguments to pass into a method
+# - still confusing: calling methods within a method (hard to understand when it's a good choice)
