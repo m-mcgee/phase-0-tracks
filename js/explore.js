4330a9aee4401254a2fb116f5de18e3bd29355fe
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-14 21:20:13 -0500

    add reverse string logic

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,18 @@
 // take in a string of characters
-// create place to store the backwards string
+// create place to store the new backwards string
 // Start at the beginning of the string
 // FOR EACH letter
-// store letter in place holder adding any previous letters behind
+// store letter in place holder adding any previous letters behind it
+
+
+var string = "Reverse Me"
+var reverseString = ""
+
+for (var i = 0; i <= string.length -1; i++) {
+	reverseString = string[i] + reverseString;
+}
+
+console.log(reverseString)
+
 
 
