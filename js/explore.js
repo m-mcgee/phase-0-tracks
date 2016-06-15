6dedd851f791ee9b4b1f65a2b23bfd47ac3f10fe
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-14 21:13:41 -0500

    initial commit

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// take in a string of characters
+// create place to store the backwards string
+// Start at the beginning of the string
+// FOR EACH letter
+// store letter in place holder adding any previous letters behind
+
+
