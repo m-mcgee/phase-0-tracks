392b5a52f424188fab08bc028dd4a1f981ed5d92
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-15 19:37:37 -0500

    add nameAndColor object

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,3 +9,17 @@ horseName.push("LeeRoy");
 
 console.log(colors);
 console.log(horseName);
+
+// Create empty object
+// IF colors array and horseName are equal length
+// 
+
+var nameAndColor = {}
+
+if (colors.length == horseName.length){
+	for (var i = 0; i < horseName.length; i++) {
+		nameAndColor[horseName[i]] = colors[i];
+	}
+}
+
+console.log(nameAndColor);
