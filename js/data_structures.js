102926af80734b4fc2ac28fcba36a38c3dd89d75
Matt McGee <matthew.m.mcgee@gmail.com>
2016-06-15 19:14:48 -0500

    add arrays with driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,11 @@
 // array of four colors
 // array of names of horses
+
+var colors = ["blue", "green", "red", "yellow"];
+var horseName = ["Ed", "Joe", "Frank", "Bob"];
+
+colors.push("orange");
+horseName.push("LeeRoy");
+
+console.log(colors);
+console.log(horseName);
