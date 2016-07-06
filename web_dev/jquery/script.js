b19d145f705cd1a99e807c3eed6b2b8199442112
Matt McGee <matthew.m.mcgee@gmail.com>
2016-07-06 00:23:11 -0500

    add slide down and slide up functionality to nav

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 
 	$("#nav-button").on("click", function(){
 
-		$("header nav ul").toggleClass("open");
+		$("header nav ul").slideDown(200);
+
+	});
+
+	$("header nav").on("mouseleave", function(){
+
+		$("header nav ul").slideUp(1000);
 
 	});
 
